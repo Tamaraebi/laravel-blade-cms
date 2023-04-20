6efffbacd04ac3332eec536082b03f2c3f300bcc
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-04-20 17:29:59 -0400

    update models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Project;
 use App\Models\Skill;
+use App\Models\Project;
 use App\Models\Education;
 use App\Models\Contact;
 use Illuminate\Database\Seeder;
@@ -22,10 +22,15 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Skill::truncate();
         Project::truncate();
+        Education::truncate();
+        Contact::truncate();
+        
         
         User::factory()->count(2)->create();
         Skill::factory()->count(3)->create();
         Project::factory()->count(4)->create();
-            
+        Contact::factory()->count(4)->create();
+        Education::factory()->count(4)->create();
+        
     }
 }
