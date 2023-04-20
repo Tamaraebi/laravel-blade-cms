2e3812ee345db97274326faeedaa4c24400a01e4
Tamara Ebi-ukuli <tamaraebiukuli@gmail.com>
2023-04-20 18:42:34 -0400

    update

--- database/migrations/2023_04_13_034237_create_education_table.php
+++ database/migrations/2023_04_13_034237_create_education_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('institution');
             $table->string('certification');
+            $table->string('description');
             $table->string('year');
             $table->string('image')->nullable();
             $table->timestamps();
